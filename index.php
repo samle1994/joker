dfbe919286f65786be3712f3fed361880db1a13b
samle1994 <58714081+samle1994@users.noreply.github.com>
2022-03-21 21:55:14 +0700

    edit

--- index.php
+++ index.php
@@ -27,25 +27,40 @@
         //print_r($arr_json);
     ?>
 
-        <!-- <main class="my-5">
-            <div class="row justify-content-center">
-                <div class="col-auto">
-                    <img id="img-0" width="200" src="img/red_joker.png" alt="joker">
+        <main class="my-5">
+            <div class="row">
+                <div class="col-8">
+                    <h1 class="mb-5 text-center">Bài 3 lá</h1>
+                    <div id="result" class="row g-4 justify-content-center ">
+
+                        <div class="col-auto">
+                            <img id="img-0" width="200" src="img/red_joker.png" alt="joker">
+                        </div>
+                        <div class="col-auto">
+                            <img id="img-1" width="200" src="img/red_joker.png" alt="joker">
+                        </div>
+                        <div class="col-auto">
+                            <img id="img-2" width="200" src="img/red_joker.png" alt="joker">
+                        </div>
+
+                    </div>
                 </div>
-                <div class="col-auto">
-                    <img id="img-1" width="200" src="img/red_joker.png" alt="joker">
-                </div>
-                <div class="col-auto">
-                    <img id="img-2" width="200" src="img/red_joker.png" alt="joker">
+                <div class="col-4">
+                    <form id="form" class="position-sticky" action="">
+                        <h2 class="mb-3 text-center">Nhập vào số người chơi</h2>
+                        <input type="number" autocomplete="off" id="number" required min="1" max="17"
+                            class="form-control" placeholder="Nhập số người chơi">
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary mt-3" id="btn-submit">Chia bài</button>
+                        </div>
+                        <button type="button" class="text-start w-100 btn btn-danger mt-3">Người thắng: <span
+                                id="win"></span> </button>
+                    </form>
                 </div>
             </div>
-            <div class="d-flex justify-content-center mt-3">
-                <button class="btn btn-primary" id="btn-submit">Chia bài</button>
-            </div>
-            <div class=" d-flex justify-content-center mt-3">
-                <button class="btn btn-danger">Số điểm là: <span id="result"></span></button>
-            </div>
-        </main> -->
+
+
+        </main>
 
         <script>
         // Chuyển json về mảng trong javascript
@@ -76,8 +91,6 @@
             return arr_1;
         }
 
-
-
         function chiabai(songuoi) {
 
             let sola = 3;
@@ -86,7 +99,7 @@
 
             let arr_news_2 = arr_2.map(x => arr_all[x]);
 
-            console.log(arr_news_2);
+            //console.log(arr_news_2);
 
             let map = new Map();
 
@@ -103,22 +116,12 @@
                     num: sum(arr_3),
                     arr: arr_3
                 };
-                map.set('num' + i, key);
+                map.set('Name_' + i, key);
             }
-            console.log(map);
+            //console.log(map);
             return map;
 
         }
-
-        let map = chiabai(3);
-        let map_news = [];
-        for (let value of map) {
-            map_news.push(value[1]['num']);
-        }
-        console.log(map_news);
-        const found = map_news.filter(element => element == 9);
-
-        console.log(found);
 
         function sum(arr_4) {
 
@@ -151,10 +154,63 @@
             return result;
         }
 
-        // document.getElementById('btn-submit').onclick = function() {
-        //     let sum_s = sum();
-        //     document.getElementById('result').innerHTML = sum_s;
-        // }
+        document.getElementById('form').onsubmit = function() {
+
+            let val = Number(document.getElementById('number').value);
+
+            if (val > 0 && val <= 17) {
+                //console.log(val);
+                let map = chiabai(val);
+                let html = '';
+                let obj = [];
+                let max = 0;
+                for (let value of map) {
+                    let arr_f = value[1]['arr'];
+                    let num_f = value[1]['num'];
+                    html += `<div class="col-6 row gx-4 gy-3 ">`;
+                    html +=
+                        `<div class="text-center"><h4>Người chơi: ${value[0]} </h4></div>`;
+                    for (value1 of arr_f) {
+                        html += `<div class="col-4">
+                            <img class="img-fluid" src="img/card/${value1}" alt="joker">
+                        </div>`;
+                    }
+                    html +=
+                        `<div><button type="button" class="w-100 btn btn-danger mt-3" >Số điểm: ${num_f} </button></div>`;
+                    html += `</div>`;
+
+                    if (num_f == '3 tây') num_f = 10;
+
+                    if (num_f > max) max = num_f;
+
+                    obj.push({
+                        name: value[0],
+                        num: num_f,
+                    });
+
+                }
+                document.getElementById('result').innerHTML = html;
+
+                // Tìm tra người thắng cuộc
+
+                let result = obj.filter(ob => ob.num == max);
+
+                let result_string = '';
+
+                for (let r of result) {
+                    result_string += r['name'] + ' ';
+                }
+                document.getElementById('win').innerHTML = result_string;
+
+                return false;
+
+            } else {
+                alert('Số người chơi lơn hơn và nhỏ hơn hoặc bằng 17');
+                document.getElementById('number').value = '';
+                return false;
+            }
+            return false;
+        }
         </script>
     </div>
 </body>
